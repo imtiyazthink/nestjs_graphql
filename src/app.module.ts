abd4a0d9dabe0b326bdd9441d29a7d98c7add15f
imtiyazthink <imtiyaz.mohammed@thinkitive.com>
2021-10-29 12:36:09 +0530

    fix : added comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ import { CustomerModule } from './customer/customer.module';
       synchronize: false,
     }),
 
+    //imported CustomerModule
     CustomerModule,
   ],
   controllers: [AppController],
